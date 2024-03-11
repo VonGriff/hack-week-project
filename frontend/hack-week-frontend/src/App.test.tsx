6704d1a16f40851a0c2308026acbdd2179371a0b
= <=>
2024-03-11 11:50:39 +0100

    setup tests and add first test

---
+++ frontend/hack-week-frontend/src/App.test.tsx
@@ -0,0 +1,9 @@
+import { describe, it } from 'vitest';
+import App from './App';
+import { render } from '@testing-library/react';
+
+describe('Test render', () => {
+  it('Should render app', () => {
+    render(<App />);
+  })
+})
